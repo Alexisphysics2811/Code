d5a5c86406fe7d1ed680e1a6b627b5028431927f
Alexisphysics2811 <alexisphysics2811@outlook.unam.mx>
2023-09-21 17:42:29 -0600

    calc switch case

--- C/Tests/calc.c
+++ C/Tests/calc.c
@@ -2,40 +2,42 @@
 Calculator
 Author: Rábago García Carlos Alexis
 Creation Date: 19/09/2023
-Last Modified: 19/09/2023
-Version: 1.0
+Last Modified: 21/09/2023
+Version: 1.1
 */
 
 #include <stdio.h>
 
 int main(){
-    int operand1, operand2, resultValue;
+    int operand1, operand2, resultValue, flag = 0;
     char operator;
     printf("Input an operation: \n");
     scanf("%d %c %d", &operand1, &operator, &operand2);
 
-    if (operator == '+'){
-        resultValue = operand1 + operand2;
-        printf("The result is: %d\n", resultValue);
+    switch(operator){
+        case '+':
+            resultValue = operand1 + operand2;
+            break;
+        case '-':
+            resultValue = operand1 - operand2;
+            break;
+        case '*':
+            resultValue = operand1 * operand2;
+            break;
+        case '/':
+            resultValue = operand1 / operand2;
+            break;
+        case '%':
+            resultValue = operand1 % operand2;
+            break;
+        default:
+            printf("Invalid operator: %c\n", operator);
+            flag = 1;
+            break;
     }
-    else if (operator == '-'){
-        resultValue = operand1 - operand2;
+
+    if (flag == 0) {
         printf("The result is: %d\n", resultValue);
-    }
-    else if (operator == '*'){
-        resultValue = operand1 * operand2;
-        printf("The result is: %d\n", resultValue);
-    }
-    else if (operator == '/'){
-        resultValue = operand1 / operand2;
-        printf("The result is: %d\n", resultValue);
-    }
-    else if (operator == '%'){
-        resultValue = operand1 % operand2;
-        printf("The result is: %d\n", resultValue);
-    }
-    else{
-        printf("Invalid operator %c\n", operator);
     }
     return 0;
 }
