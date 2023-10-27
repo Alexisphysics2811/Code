0b3824d1333c4e766fbc4109d7f7785789ea0724
Alexisphysics2811 <alexisphysics2811@outlook.unam.mx>
2023-10-26 18:29:28 -0600

    quick fix

--- Python/Projects/P07/matrixprod.py
+++ Python/Projects/P07/matrixprod.py
@@ -3,7 +3,7 @@ Flexible Matrix Product
 Author: Rábago García Carlos Alexis
 Creation Date: 24/10/2023
 Last Modified: 24/10/2023
-Version: 1.0
+Version: 1.0.1
 """
 
 import numpy as np
@@ -25,6 +25,6 @@ print(f"Enter the elements separated by spaces ({M}x{B_m}): ")
 b = list(map(int, input().split()))
 
 mtrx_B = np.array(b).reshape(M, B_m)
-print(f"A ({M}x{B_m}) = \n{mtrx_B}\n")
+print(f"B ({M}x{B_m}) = \n{mtrx_B}\n")
 
 print(f"AB ({A_n}x{B_m}) = \n{np.dot(mtrx_A, mtrx_B)}")
